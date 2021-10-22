a8f9442a51b4da6188744b18a55c868128b187e6
yuyang <yang.yu@goease.com>
2021-10-22 11:28:31 +0800

    jwt登录

--- src/main/java/com/xiao9/user/facade/rest/UserLoginController.java
+++ src/main/java/com/xiao9/user/facade/rest/UserLoginController.java
@@ -1,15 +1,11 @@
 package com.xiao9.user.facade.rest;
 
+import com.xiao9.user.application.service.LoginService;
 import com.xiao9.user.facade.rest.view.LoginDTO;
 import com.xiao9.user.infrastruction.security.jwt.JWTFilter;
-import com.xiao9.user.infrastruction.security.jwt.TokenProvider;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,24 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserLoginController {
 
-    private final TokenProvider tokenProvider;
+    private final LoginService loginService;
 
-    private AuthenticationManagerBuilder authenticationManagerBuilder;
-
-    public UserLoginController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
-        this.tokenProvider = tokenProvider;
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    public UserLoginController(LoginService loginService) {
+        this.loginService = loginService;
     }
 
 
+    /**
+     * 用户通过用户名密码登录获取jwt
+     * @param login 登录名或者邮箱
+     * @return jwtToken
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<JWTToken> authorize(@RequestBody LoginDTO login) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                login.getUsername(), login.getPassword());
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        boolean remember = login.getRememberMe() == null ? false : login.getRememberMe();
-        String jwt = tokenProvider.createToken(authentication, remember);
+        String jwt = loginService.getJWTToken(login);
         HttpHeaders headers = new HttpHeaders();
         headers.add(JWTFilter.AUTHORIZATION_HEADER, jwt);
         return new ResponseEntity<>(new JWTToken(jwt), headers, HttpStatus.OK);
